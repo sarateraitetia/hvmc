714a60f9fcf4f5ef2ee3b7b0781db22279355ed1
william00007 <byakuraii172@gmail.com>
2015-12-07 17:49:36 +0100

    Update hvmc_physics.h

--- src/hvmc_physics.h
+++ src/hvmc_physics.h
@@ -5,6 +5,7 @@
 #include "hvmc_math.h"
 #include "hvmc_collisions.h"
 
+
 enum RigidBodyType
 {
     RIGID_BODY_SPHERE,
@@ -18,63 +19,66 @@ struct Collider
 
     union
     {
-        f32 radius; // Sphere
+        f32 radius; // Sphere rayon
         vec2 dims; // Box
     };
 };
+
+
 
 struct RigidBody
 {
     RigidBody() {}
     ~RigidBody() {}
-    
-    void Update( f32 dt ); // MAJ de la vitesse et de la position 
-    
+
+    void Update( f32 dt ); // MAJ de la vitesse et de la position
+
     void ApplyForce( vec2 const& force ); // A FAIRE !!
     void ApplyImpulse( vec2 const& impulse, vec2 const& contactVector ); // A FAIRE !!
     // pour le collisions (plus tard)
-    
+
     void SetKinematic(); // pour les murs, mais la masse et l'inverse de
     // la masse à 0
     void SetGravityMode( int mode ); // on s'en fout
 
 
     // ------------------------------------------------ SARA
-    void IntegradeForces(f32 dt);
-    void IntegradeVelocities(f32 dt);
+    void IntegrateForces(f32 dt);
+    void IntegrateVelocities(f32 dt);
     // -----------------------------------------------------
 
 
-    
+
     f32 I = 0.f;  // inertia -> masse mais pour les rotations
-    f32 iI = 0.f; // inverse inertia est précalculé 
+    f32 iI = 0.f; // inverse inertia est précalculé
     // 1) on n'aime pas les divisions en info
     // 2) on veut pas que les objets bougent (= masse de 0)
     f32 m = 0.f;  // mass
     f32 im = 0.f; // inverse mass
-    
+
     int gravityMode = 1; // objet tombe ou pas
     // on peut l'ignorer en disant que les objets tombent par défaut
 
     vec2 forces;
     vec2 position;
-    vec2 velocity;
+    vec2 velocity; // vitesse
 
     f32 torque = 0.f;
-    f32 rotation = 0.f; 
-    f32 angularVelocity = 0.f;
+    f32 rotation = 0.f;
+    f32 angularVelocity = 0.f; // vitesse angulaire
 
     Collider collider;
 };
+
 
 struct PhysicsSystem // moteur physique
 {
     bool Init(); // mets la gravité à 0.9
     void Cleanup();
-    
+
     void Update( f32 dt ); // A FAIRE !!
     // à appeler sur les rigidBodies
-    
+
     // Déjà écris - ne pas toucher sauf pour changer la massed 'un objet
     RigidBody* AddSphere( vec2 const& pos, f32 radius );
     RigidBody* AddBox( vec2 const& pos, vec2 const& dims );
@@ -82,6 +86,10 @@ struct PhysicsSystem // moteur physique
 
     std::vector<RigidBody*> rigidBodies;
     vec2 gravity;
+
+    // ------------------------------------------ SARA
+
+    // ------------------------------------------------
 };
 
 #endif
