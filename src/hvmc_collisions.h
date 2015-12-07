7a9a20779f4d31f22527ed96abf3ac61dfa1cc5f
william00007 <byakuraii172@gmail.com>
2015-12-07 17:48:47 +0100

    Update hvmc_collisions.h

--- src/hvmc_collisions.h
+++ src/hvmc_collisions.h
@@ -3,5 +3,30 @@
 
 #include "hvmc_math.h"
 
+struct RigidBody;
+
+struct CollisionInfo{
+    RigidBody *a; // 2 RigidBody en collision
+    RigidBody *b;
+
+    vec2 normal; // la normale au contact
+    f32 penetration; // distance de pénétration entre les objets
+
+    vec2 contactPoint; // point de contact
+
+    f32 coefRestitution; // coefficient de restitution (entre 0 et 1)
+
+    void Solve() const;
+    void CorrectPositions() const;
+};
+
+
+bool collide(RigidBody* a,RigidBody* b, CollisionInfo &info);
+bool collideSphereSphere(RigidBody* a, RigidBody* b, CollisionInfo& info);
+bool collideBoxBox(RigidBody* a, RigidBody* b, CollisionInfo& info);
+bool collideBoxSphere(RigidBody* b, RigidBody* a, CollisionInfo& info);
+
+
+
 #endif
 
