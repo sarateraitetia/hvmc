dda5dd44269457986753b189738bf8ebfc4e589e
william00007 <byakuraii172@gmail.com>
2015-12-13 23:31:28 +0100

    Update hvmc_physics.h

--- src/hvmc_physics.h
+++ src/hvmc_physics.h
@@ -5,7 +5,6 @@
 #include "hvmc_math.h"
 #include "hvmc_collisions.h"
 
-
 enum RigidBodyType
 {
     RIGID_BODY_SPHERE,
@@ -19,77 +18,59 @@ struct Collider
 
     union
     {
-        f32 radius; // Sphere rayon
+        f32 radius; // Sphere
         vec2 dims; // Box
     };
 };
-
-
 
 struct RigidBody
 {
     RigidBody() {}
     ~RigidBody() {}
+    
+    void Update( f32 dt );
+    
+    void ApplyForce( vec2 const& force );
+    void ApplyImpulse( vec2 const& impulse, vec2 const& contactVector );
+    
+    void SetKinematic();
+    void SetGravityMode( int mode );
 
-    void Update( f32 dt ); // MAJ de la vitesse et de la position
-
-    void ApplyForce( vec2 const& force ); // A FAIRE !!
-    void ApplyImpulse( vec2 const& impulse, vec2 const& contactVector ); // A FAIRE !!
-    // pour le collisions (plus tard)
-
-    void SetKinematic(); // pour les murs, mais la masse et l'inverse de
-    // la masse à 0
-    void SetGravityMode( int mode ); // on s'en fout
-
-
-    // ------------------------------------------------ SARA
-    void IntegrateForces(f32 dt);
-    void IntegrateVelocities(f32 dt);
-    // -----------------------------------------------------
-
-
-
-    f32 I = 0.f;  // inertia -> masse mais pour les rotations
-    f32 iI = 0.f; // inverse inertia est précalculé
-    // 1) on n'aime pas les divisions en info
-    // 2) on veut pas que les objets bougent (= masse de 0)
+    void IntegrateForces( f32 dt);
+    void IntegrateVelocities( f32 dt);
+    
+    f32 I = 0.f;  // inertia
+    f32 iI = 0.f; // inverse inertia
     f32 m = 0.f;  // mass
     f32 im = 0.f; // inverse mass
-
-    int gravityMode = 1; // objet tombe ou pas
-    // on peut l'ignorer en disant que les objets tombent par défaut
+    f32 restitution = 0.5f;
+    
+    int gravityMode = 1;
 
     vec2 forces;
     vec2 position;
-    vec2 velocity; // vitesse
+    vec2 velocity;
 
     f32 torque = 0.f;
-    f32 rotation = 0.f;
-    f32 angularVelocity = 0.f; // vitesse angulaire
+    f32 rotation = 0.f; 
+    f32 angularVelocity = 0.f;
 
     Collider collider;
 };
 
-
-struct PhysicsSystem // moteur physique
+struct PhysicsSystem
 {
-    bool Init(); // mets la gravité à 0.9
+    bool Init();
     void Cleanup();
-
-    void Update( f32 dt ); // A FAIRE !!
-    // à appeler sur les rigidBodies
-
-    // Déjà écris - ne pas toucher sauf pour changer la massed 'un objet
+    
+    void Update( f32 dt );
+    
     RigidBody* AddSphere( vec2 const& pos, f32 radius );
     RigidBody* AddBox( vec2 const& pos, vec2 const& dims );
     RigidBody* AddWall( vec2 const& pos, vec2 const& dims );
 
     std::vector<RigidBody*> rigidBodies;
     vec2 gravity;
-
-    // ------------------------------------------ SARA
-
-    // ------------------------------------------------
 };
 
 #endif
